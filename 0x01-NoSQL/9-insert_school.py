2f50ba41d3dfd013a765858cb868a33e8e4a5745
onokajames96 <jamesonoka996@gmail.com>
2024-03-26 00:43:06 -0700

    insertion of document

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A function that inserts a new document in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Returns the new _id
+    """
+    new_docs = mongo_collection.insert_one(kwargs)
+    return new_docs.inserted_id
